920f1cffc096f543d138b51d8d18f14d62c171f9
rachael-ann <sylvia.scarlett.1935@gmail.com>
2017-04-22 14:43:47 -0700

    twitter feed

--- js/main.js
+++ js/main.js
@@ -61,6 +61,32 @@ function getDateTime() {
   $('#time').html(time);
 }
 
+function loadTweets() {
+  $.ajax({
+    url: '../php/twitter.php',
+    type: 'GET',
+    success: function(response) {
+      if (typeof response.errors === 'undefined' || response.errors.length < 1) {
+        var $tweets = $('<ul class="list-unstyled tweets px-5"></ul>');
+        $.each(response, function(i, obj) {
+          var $tweet = '<img src="' + obj.user.profile_image_url + '">';
+          $tweet += '<strong>' + obj.user.name + '</strong>';
+          $tweet += '@' + obj.user.screen_name;
+          $tweet += '<small class="text-muted">' + moment(obj.created_at).fromNow() + '</small>'
+          $tweet += '<span class="text">' + obj.text + '</span>';
+          $tweets.append('<li class="tweet">' + $tweet + '</li>');
+        });
+        $('#twitter-content .content').html($tweets);
+      } else {
+        $('#twitter-content .content').text('Response error');
+      }
+    },
+    error: function(errors) {
+      $('#twitter-content .content').text('Request error');
+    }
+  });
+}
+
 $('#spotify').click(openSpotify);
 $('#youtube').click(openYoutube);
 $('#twitter').click(openTwitter);
@@ -72,7 +98,9 @@ $('#close-twitter').click(closeTwitter);
 $(document).ready(function() {
   getDateTime();
   getWeather();
+  loadTweets();
 
   setInterval(getDateTime, 1000);
   setInterval(getWeather, 1000);
+  setInterval(loadTweets, 60000);
 });
